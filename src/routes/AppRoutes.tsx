439a5eb70378e6f5cc752973c637f207a71626cc
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-08-29 15:56:28 +0300

    remove lazy loaded cart page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,8 @@
-import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Product from '../pages/Product';
-import Spinner from '../components/Spinner';
-const Cart = lazy(() => import('../pages/Cart'));
+import Cart from '../pages/Cart';
 
 const AppRoutes = () => {
     return (
@@ -12,14 +10,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/product/:id" element={<Product />} />
-                <Route
-                    path="/cart"
-                    element={
-                        <Suspense fallback={<Spinner />}>
-                            <Cart />
-                        </Suspense>
-                    }
-                />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </>
     );
