0328d343f8c60200ddb579477d1123b232b73fd1
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-08-29 15:50:41 +0300

    add spinner

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,10 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Product from '../pages/Product';
-import Cart from '../pages/Cart';
+import Spinner from '../components/Spinner';
+const Cart = lazy(() => import('../pages/Cart'));
 
 const AppRoutes = () => {
     return (
@@ -10,7 +12,14 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/product/:id" element={<Product />} />
-                <Route path="/cart" element={<Cart />} />
+                <Route
+                    path="/cart"
+                    element={
+                        <Suspense fallback={<Spinner />}>
+                            <Cart />
+                        </Suspense>
+                    }
+                />
             </Routes>
         </>
     );
