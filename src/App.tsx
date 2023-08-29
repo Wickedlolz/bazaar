558f6f1f90db497dda3a9ca6457f64a06c0c9c99
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-08-29 10:43:15 +0300

    add AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
 
-import { useAppDispatch } from "./store";
-import { fetchProducts } from "./store/reducers/productsSlice";
+import { useAppDispatch } from './store';
+import { fetchProducts } from './store/reducers/productsSlice';
 
-import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
+import MainLayout from './layouts/MainLayout';
+import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
 
-  return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
-      </Routes>
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <AppRoutes />
+        </MainLayout>
+    );
 };
 
 export default App;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Product from '../pages/Product';
+import Cart from '../pages/Cart';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
