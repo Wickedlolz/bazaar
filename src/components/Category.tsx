0ed90fae9dbd460882dc097bade7baed05034dd9
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-01 07:46:30 +0300

    extract category items in separeta file

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,44 +1,30 @@
 import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../store';
+import { changeCategory } from '../store/reducers/productsSlice';
+
+import { items } from '../utils/categoryData';
 
 import NavTitle from './NavTitle';
 import { ImPlus } from 'react-icons/im';
 
 const Category = () => {
+    const dispatch = useAppDispatch();
+    const { selectedCategory } = useAppSelector((state) => state.bazaar);
     const [showSubCatOne, setShowSubCatOne] = useState(false);
-    const items = [
-        {
-            _id: 990,
-            title: 'New Arrivals',
-            icons: true,
-        },
-        {
-            _id: 991,
-            title: 'Gudgets',
-        },
-        {
-            _id: 992,
-            title: 'Accessories',
-            icons: true,
-        },
-        {
-            _id: 993,
-            title: 'Electronics',
-        },
-        {
-            _id: 994,
-            title: 'Others',
-        },
-    ];
 
     return (
         <div className="w-full">
             <NavTitle title="Shop by Category" icons={false} />
             <div>
                 <ul className="flex flex-col gap-4 text-sm lg:text-base text-[#767676]">
-                    {items.map(({ _id, title, icons }) => (
+                    {items.map(({ _id, title, category, icons }) => (
                         <li
                             key={_id}
-                            className="border-b-[1px] border-b-[#F0F0F0] pb-2 flex items-center justify-between"
+                            onClick={(e) => dispatch(changeCategory(category))}
+                            className={`border-b-[1px] border-b-[#F0F0F0] pb-2 flex items-center justify-between cursor-pointer ${
+                                selectedCategory === category &&
+                                'text-orange-900'
+                            }`}
                         >
                             {title}
                             {icons && (
---
+++ src/utils/categoryData.ts
@@ -0,0 +1,30 @@
+import { ICategory } from '../interfaces/categoryItems';
+
+export const items: ICategory[] = [
+    {
+        _id: 989,
+        title: 'All',
+        category: 'all',
+    },
+    {
+        _id: 990,
+        title: 'Women',
+        category: 'women',
+    },
+    {
+        _id: 991,
+        title: 'Men',
+        category: 'men',
+    },
+    {
+        _id: 992,
+        title: 'Electronics',
+        category: 'electronics',
+    },
+    {
+        _id: 993,
+        title: 'Others',
+        category: 'other',
+        icons: true,
+    },
+];
