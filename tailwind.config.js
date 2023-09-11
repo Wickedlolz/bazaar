656637759c0509b86c110370fbc88388b0f42118
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-11 14:17:57 +0300

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     darkMode: 'class',
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primeColor: '#262626',
+            },
+        },
     },
     plugins: [],
 };
