d9f45f5cff9de7b437c46969405d5308203fca8e
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-14 15:44:05 +0300

    add route for login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -12,6 +12,7 @@ import Cart from '../pages/Cart';
 import Shop from '../pages/Shop';
 import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
+import Login from '../pages/Login';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
             <Route path="/product/:id" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/shop" element={<Shop />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<NotFound />} />
         </Route>
@@ -31,29 +33,3 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
-
-// import { Routes, Route } from 'react-router-dom';
-
-// import Home from '../pages/Home';
-// import Product from '../pages/Product';
-// import Cart from '../pages/Cart';
-// import Login from '../pages/Login';
-// import Shop from '../pages/Shop';
-// import NotFound from '../pages/NotFound';
-
-// const AppRoutes = () => {
-//     return (
-//         <>
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/product/:id" element={<Product />} />
-//                 <Route path="/cart" element={<Cart />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/shop" element={<Shop />} />
-//                 <Route path="*" element={<NotFound />} />
-//             </Routes>
-//         </>
-//     );
-// };
-
-// export default AppRoutes;
