964f6d9dca19b6eebf47fcc07913923f2d030573
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-28 18:30:40 +0300

    fix: alt names

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -51,28 +51,28 @@ const Banner = () => {
             <Slider {...settings}>
                 <img
                     src={BannerImg1}
-                    alt="Banner  1"
+                    alt="Banner 1"
                     className="w-screen h-full object-cover"
                     width={1000}
                     height={500}
                 />
                 <img
                     src={BannerImg2}
-                    alt="Banner  2"
+                    alt="Banner 2"
                     className="w-screen h-full object-cover"
                     width={1000}
                     height={500}
                 />
                 <img
                     src={BannerImg3}
-                    alt="Banner img 3"
+                    alt="Banner 3"
                     className="w-screen h-full object-cover"
                     width={1000}
                     height={500}
                 />
                 <img
                     src={BannerImg4}
-                    alt="Banner img 4"
+                    alt="Banner 4"
                     className="w-screen h-full object-cover"
                     width={1000}
                     height={500}
