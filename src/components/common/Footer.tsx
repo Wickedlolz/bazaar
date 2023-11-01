44b9f3409afebdbd91fda84274093d0670eb8c80
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-01 11:24:28 +0200

    fix responsive

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -18,87 +18,87 @@ const Footer = () => {
     const intl = useIntl();
 
     return (
-        <footer className="bg-black text-[#949494] py-20 px-20">
-            <div className="max-w-screen-xl mx-10 grid grid-cols-4">
-                <div className="flex flex-col gap-7">
-                    <h2 className="font-bold space-x-1 text-white text-4xl line-through">
+        <footer className='bg-black text-[#949494] py-20 md:px-20'>
+            <div className='md:max-w-screen-xl mx-10 grid grid-cols-1 gap-3 md:gap-0 md:grid-cols-4'>
+                <div className='flex flex-col gap-7'>
+                    <h2 className='font-bold space-x-1 text-white text-4xl line-through'>
                         {APP_TITLE}
                     </h2>
-                    <p className="text-white text-sm tracking-wide">
+                    <p className='text-white text-sm tracking-wide'>
                         &copy; {APP_TITLE} 2023
                     </p>
                     <img
-                        className="w-56 object-cover"
+                        className='w-56 object-cover'
                         src={PaymentLogo}
-                        alt="paymentLogo"
+                        alt='paymentLogo'
                     />
-                    <div className="flex gap-5 text-lg text-gray-400">
-                        <ImGithub className="hover:text-white duration-300 cursor-pointer" />
-                        <FaYoutube className="hover:text-white duration-300 cursor-pointer" />
-                        <FaFacebookF className="hover:text-white duration-300 cursor-pointer" />
-                        <FaTwitter className="hover:text-white duration-300 cursor-pointer" />
-                        <FaInstagram className="hover:text-white duration-300 cursor-pointer" />
+                    <div className='flex gap-5 text-lg text-gray-400'>
+                        <ImGithub className='hover:text-white duration-300 cursor-pointer' />
+                        <FaYoutube className='hover:text-white duration-300 cursor-pointer' />
+                        <FaFacebookF className='hover:text-white duration-300 cursor-pointer' />
+                        <FaTwitter className='hover:text-white duration-300 cursor-pointer' />
+                        <FaInstagram className='hover:text-white duration-300 cursor-pointer' />
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-2xl font-semibold text-white mb-4">
-                        <FormattedMessage id="footer_locate_us_lbl" />
+                    <h2 className='text-2xl font-semibold text-white mb-4'>
+                        <FormattedMessage id='footer_locate_us_lbl' />
                     </h2>
-                    <div className="text-base flex flex-col gap-2">
+                    <div className='text-base flex flex-col gap-2'>
                         <p>MBD, Ruwi, Muscat-Onan</p>
                         <p>
-                            <FormattedMessage id="footer_mobile" />: 00968
+                            <FormattedMessage id='footer_mobile' />: 00968
                             97859628
                         </p>
                         <p>
-                            <FormattedMessage id="footer_phone" />: 00968
+                            <FormattedMessage id='footer_phone' />: 00968
                             24769821
                         </p>
                         <p>
-                            <FormattedMessage id="footer_email" />:
+                            <FormattedMessage id='footer_email' />:
                             bazaar@gmail.com
                         </p>
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-2xl font-semibold text-white mb-4">
-                        <FormattedMessage id="footer_profile" />
+                    <h2 className='text-2xl font-semibold text-white mb-4'>
+                        <FormattedMessage id='footer_profile' />
                     </h2>
-                    <div className="flex flex-col gap-2 text-base">
-                        <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                    <div className='flex flex-col gap-2 text-base'>
+                        <p className='flex items-center gap-3 hover:text-white duration-300 cursor-pointer'>
                             <span>
                                 <BsPersonFill />
                             </span>
-                            <FormattedMessage id="footer_my_account" />
+                            <FormattedMessage id='footer_my_account' />
                         </p>
-                        <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <p className='flex items-center gap-3 hover:text-white duration-300 cursor-pointer'>
                             <span>
                                 <BsPaypal />
                             </span>
-                            <FormattedMessage id="footer_checkout" />
+                            <FormattedMessage id='footer_checkout' />
                         </p>
-                        <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <p className='flex items-center gap-3 hover:text-white duration-300 cursor-pointer'>
                             <span>
                                 <FaHome />
                             </span>
-                            <FormattedMessage id="footer_order_tracking" />
+                            <FormattedMessage id='footer_order_tracking' />
                         </p>
-                        <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <p className='flex items-center gap-3 hover:text-white duration-300 cursor-pointer'>
                             <span>
                                 <MdLocationOn />
                             </span>
-                            <FormattedMessage id="footer_help_and_support" />
+                            <FormattedMessage id='footer_help_and_support' />
                         </p>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center">
+                <div className='flex flex-col justify-center'>
                     <input
-                        className="bg-transparent border px-4 py-2 text-sm"
+                        className='bg-transparent border px-4 py-2 text-sm'
                         placeholder={intl.formatMessage({ id: 'footer_email' })}
-                        type="text"
+                        type='text'
                     />
-                    <button className="text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black">
-                        <FormattedMessage id="footer_subscribe" />
+                    <button className='text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black'>
+                        <FormattedMessage id='footer_subscribe' />
                     </button>
                 </div>
             </div>
