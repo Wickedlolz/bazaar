d9a3085e7e7409832a69ab82a307c365c1466204
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-09 19:00:07 +0200

    add form and handle sign in with email and pass ✅

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,18 @@
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFirebaseContext } from '../contexts/FirebaseContext';
 import { Helmet } from 'react-helmet-async';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { toast } from 'react-toastify';
 
 import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
-    const { signInWithGoogle } = useFirebaseContext();
+    const { signInWithGoogle, signIn } = useFirebaseContext();
     const navigate = useNavigate();
     const intl = useIntl();
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     /**
      * Handles the process of user authentication through Google sign-in.
@@ -23,23 +27,77 @@ const Login = () => {
         navigate('/');
     };
 
+    /**
+     * Handles the form submission event.
+     *
+     * @param {FormEvent<HTMLFormElement>} event - The form submission event.
+     */
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (email.length === 0 || password.length === 0) {
+            toast.error('All fields are required!');
+            return;
+        }
+
+        try {
+            await signIn(email, password);
+            navigate('/');
+        } catch (error) {
+            const { message } = error as { message: string };
+            toast.error(message);
+        }
+    };
+
     return (
         <section className="w-full flex flex-col dark-theme items-center justify-center gap-10 py-20">
             <Helmet>
                 <title>
-                    Login {intl.formatMessage({ id: 'page_title' }) || ''}
+                    Sign In {intl.formatMessage({ id: 'page_title' }) || ''}
                 </title>
             </Helmet>
-            <div className="w-full flex items-center justify-center gap-10">
-                <div
+            <div className="w-full flex flex-col items-center justify-center gap-8">
+                <button
                     onClick={handleGoogleLogin}
-                    className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
+                    className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 duration-300"
                 >
                     <FcGoogle className="w-8" />
                     <span className="text-sm text-gray-900 dark:text-white">
                         <FormattedMessage id="auth_sign_in_with_google" />
                     </span>
-                </div>
+                </button>
+                <p>OR</p>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col gap-3 border-[1px] rounded-lg shadow-md p-4"
+                >
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className="outline-none bg-slate-200 p-2 rounded-md"
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email..."
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        className="outline-none bg-slate-200 p-2 rounded-md"
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Password..."
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="bg-blue-500 p-3 rounded-lg text-white hover:bg-blue-400 duration-300 mt-3"
+                    >
+                        Sign In
+                    </button>
+                </form>
             </div>
         </section>
     );
