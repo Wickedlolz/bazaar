5398e5b010e42adf44bb4f0892a2d2dc17ebe633
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-09 18:58:54 +0200

    add default image when user dont have photo ✅

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -41,18 +41,26 @@ const Header = () => {
                                 ? 'border-[2px] border-orange-800 p-1 rounded-full'
                                 : ''
                         }
-                        to={user ? '/profile' : '/login'}
+                        to={user ? '/profile' : '/sign-in'}
                     >
-                        <img
-                            className="rounded-full"
-                            src={
-                                user?.photoURL ||
-                                'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80'
-                            }
-                            width={30}
-                            height={30}
-                            alt="User Profile"
-                        />
+                        {user?.photoURL ? (
+                            <img
+                                className="rounded-full"
+                                src={user?.photoURL}
+                                width={30}
+                                height={30}
+                                alt="User Profile"
+                            />
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="28"
+                                height="28"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z" />
+                            </svg>
+                        )}
                     </NavLink>
                     {user && (
                         <p className="text-base font-semibold underline">
